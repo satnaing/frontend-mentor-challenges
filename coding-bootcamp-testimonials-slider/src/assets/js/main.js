1fa03993c21de9c3d07deda4214bf937639e653d
Sat Naing <satnaingdev@gmail.com>
2021-07-24 11:07:59 +0630

    SwiperJS implemented

--- coding-bootcamp-testimonials-slider/src/assets/js/main.js
+++ coding-bootcamp-testimonials-slider/src/assets/js/main.js
@@ -3,24 +3,15 @@ import Swiper from "swiper";
 // import Swiper styles
 import "swiper/swiper-bundle.css";
 
-const swiper = new Swiper(".swiper-container", {
-  // Optional parameters
-  // direction: "vertical",
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
-
-  // Navigation arrows
+var swiper = new Swiper(".mySwiper", {
+  cssMode: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
+  pagination: {
+    el: ".swiper-pagination",
   },
+  mousewheel: true,
+  keyboard: true,
 });
